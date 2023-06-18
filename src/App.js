18c7047ea534d2a364e59cfd5b2634ad16d6c4b1
Ishita Kumari <ishitakumaripakur@gmail.com>
2023-06-19 00:03:44 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import ColorPicker from './ColorPicker';
 
 function App() {
+  
+  const colors = ['aqua', 'blueviolet', 'chocolate','yellow','black','red','blue','aquamarine'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ColorPicker colors={colors}/>
+    </>
   );
 }
 
